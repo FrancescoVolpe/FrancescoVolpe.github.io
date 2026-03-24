b550abf1f1de151953e570e206f18694a3134cdd
Francesco Volpe <francesco.volpe1998@gmail.com>
2026-03-24 10:08:38 +0100

    navbar complete remake, toggle in mobile and expanded in desktop

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.getElementById("menu-toggle");
-    const menu = document.querySelector(".menu");
+    const menu = document.getElementById("menu");
 
+    // Toggle apertura/chiusura
     toggle.addEventListener("click", (e) => {
         e.preventDefault();
         menu.classList.toggle("active");
     });
 
+    // Chiusura cliccando fuori
     document.addEventListener("click", (e) => {
-        if (!menu.contains(e.target)) {
-            menu.classList.remove("active");
+        const isClickInsideMenu = menu.contains(e.target);
+        const isClickOnToggle = toggle.contains(e.target);
+
+        if (!isClickInsideMenu && !isClickOnToggle) {
+        menu.classList.remove("active");
         }
     });
 });
